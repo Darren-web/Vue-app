c082e43a49d7e195eb2187c07c400a37d894e2fd
Darrem-web <1979414630@qq.com>
2020-05-22 17:36:42 +0800

    icons-one

--- src/components/HomeSwiper.vue
+++ src/components/HomeSwiper.vue
@@ -40,7 +40,7 @@
         overflow hidden
         width 100%
         height 0
-        padding-bottom 31.25%
+        padding-bottom 30.25%
         background #cccccc
         .swiper-img
             width 100%
