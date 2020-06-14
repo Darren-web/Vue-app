a1a8e1b1b9e0754566cea6c70b2482ab7facee4d
Darrem-web <1979414630@qq.com>
2020-06-14 10:31:11 +0800

    city-ajax

--- src/views/City.vue
+++ src/views/City.vue
@@ -2,23 +2,47 @@
     <div>
         <CityHeader/>
         <CitySearch/>
-        <CityList/>
-        <CityAlphabet/>
+        <CityList :cities="cities" :hotCities="hotCities"/>
+        <CityAlphabet :cities="cities"/>
     </div>
 </template>
 
 <script>
+    import axios from 'axios'
     import CityHeader from './../components/city/CityHeader'
     import CitySearch from './../components/city/CitySearch'
     import CityList from './../components/city/CityList'
     import CityAlphabet from './../components/city/CityAlphabet'
     export default {
         name:"City",
+        data() {
+            return{
+                cities:{},
+                hotCities:[]
+            }
+        },
         components:{
             CityHeader,
             CitySearch,
             CityList,
             CityAlphabet
+        },
+        methods:{
+            getCityInfo() {
+                axios.get("/mock/city.json")
+                .then(this.handleGetCityInfoSucc)
+            },
+            handleGetCityInfoSucc(res) {
+                res = res.data
+                if(res.ret && res.data){
+                    const data = res.data
+                    this.cities = data.cities
+                    this.hotCities = data.hotCities
+                }
+            }
+        },
+        mounted() {
+            this.getCityInfo()
         }
     }
 </script>
